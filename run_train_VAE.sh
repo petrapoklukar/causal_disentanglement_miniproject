2a892cb9d8b0d83961380f0fdc4c432ecb726603
petrapoklukar <petra.poklukar@gmail.com>
2020-03-29 14:52:40 +0200

    Update experiments

--- run_train_VAE.sh
+++ run_train_VAE.sh
@@ -19,7 +19,7 @@ mkdir -p $RUNS_PATH
 #SBATCH --error="${RUNS_PATH}/%J_slurm.err"
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user="poklukar${AT}kth.se"
-#SBATCH --constrain="khazadum|rivendell|belegost|shire|gondor"
+#SBATCH --constrain="khazadum|rivendell|shire|gondor"
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40GB
