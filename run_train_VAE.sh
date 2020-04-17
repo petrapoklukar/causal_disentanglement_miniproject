a8fce7baf5cae210b756a7fa6bc5a66595e36c91
petrapoklukar <petra.poklukar@gmail.com>
2020-04-17 21:37:02 +0200

    running noncausal vaes

--- run_train_VAE.sh
+++ run_train_VAE.sh
@@ -7,7 +7,7 @@ AT="@"
 # SBATCH_OR_CAT=cat
 SBATCH_OR_CAT=sbatch
 
-for config in "VAE_CausalDsprite_ber_shape2_scale5_ld2"; do
+for config in "VAE_NonCausalDsprite_ber_shape2_scale5_ld2"; do
 
 RUNS_PATH="${SOURCE_PATH}/models/${config}"
 echo $RUNS_PATH
